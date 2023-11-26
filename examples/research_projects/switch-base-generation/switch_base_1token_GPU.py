a2efcf3538ba0814643f7881e79cfdd2b0449ba2
Yeeethan00 <yirj0809@gmail.com>
2023-11-26 13:32:39 +0800

    fix: outline & 4-bit list

--- examples/research_projects/switch-base-generation/switch_base_1token_GPU.py
+++ examples/research_projects/switch-base-generation/switch_base_1token_GPU.py
@@ -9,6 +9,9 @@ ExpertManager.CacheLimit = 12
 ExpertManager.FTpwd = "-samsum"
 base_num = 8
 
+
+
+ExpertManager.bwList = ['2_0', '3_5', '0_0', '0_5', '5_5', '4_0', '5_0', '4_5', '1_0', '4_3', '2_3', '3_3', '1_5', '3_2', '0_2', '0_3', '2_2', '4_2', '2_5', '3_0', '5_2', '1_2', '1_3', '5_3', '3_6', '4_6', '0_6', '2_6', '2_4', '3_4', '1_6', '5_6', '1_4', '5_1', '1_1', '3_1', '0_4', '0_1', '1_7', '4_4', '2_1', '4_1', '2_7', '5_7', '5_4', '4_7', '0_7', '3_7']
 if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/models/switch-base-"+str(base_num)+ExpertManager.FTpwd+"-yrj")
     model = SwitchTransformersModelYRJ.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/models/switch-base-"+str(base_num)+ExpertManager.FTpwd+"-yrj", device_map="auto", offload_folder="offload")
@@ -34,3 +37,4 @@ if __name__ == '__main__':
     peak_memory = torch.cuda.max_memory_allocated()  # Get peak GPU memory usage
     print("Inference Time:", time_, "s")
     print("Peak GPU Memory Usage:", peak_memory / 1024 ** 2, "MB")  # Print peak GPU memory usage
+    print("Peak GPU Memory Usage:", peak_memory / 1024 ** 3, "GB")  # Print peak GPU memory usage
--- examples/research_projects/switch-base-generation/switch_base_metric.py
+++ examples/research_projects/switch-base-generation/switch_base_metric.py
@@ -47,29 +47,32 @@ ExpertManager.Device = "cuda"
 ExpertManager.CacheLimit = 12
 ExpertManager.FTpwd = "-samsum"
 # ExpertManager.FTpwd = ""
-tokenizer = AutoTokenizer.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/models/switch-base-8"+ExpertManager.FTpwd+"-yrj")
-model = SwitchTransformersForConditionalGenerationYRJ.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/models/switch-base-8"+ExpertManager.FTpwd+"-yrj", device_map="auto", offload_folder="offload")
 
-output_lns= []
-reference_lns = []
-sum_cont= 0
-for i, input in enumerate(inputSens):
-    if sum_cont >= 200:
-        break
-    # input_text = "summarize: "+input[textt]
-    input_text = input[textt]
-    if len(input_text.split(" "))< 300:
-        label_text = input["summary"]
-        input_ids = tokenizer(input_text, return_tensors="pt").input_ids
-        input_ids = input_ids.to('cuda:0')
-        with torch.no_grad():  # Avoid saving intermediate layer outputs
-            outputs = model.generate(input_ids, max_new_tokens=100)
-        output_text = tokenizer.decode(outputs[0])
-        output_text=output_text.replace("<pad> ","")
-        output_text=output_text.replace("</s>","")
-        output_lns.append(output_text)
-        reference_lns.append(label_text)
-        sum_cont += 1
-result = calculate_rouge(output_lns, reference_lns, use_stemmer=True)
-print(result)
-print("total cnt:", sum_cont)
+ExpertManager.bwList = ['2_0', '3_5', '0_0', '0_5', '5_5', '4_0', '5_0', '4_5', '1_0', '4_3', '2_3', '3_3', '1_5', '3_2', '0_2', '0_3', '2_2', '4_2', '2_5', '3_0', '5_2', '1_2', '1_3', '5_3', '3_6', '4_6', '0_6', '2_6', '2_4', '3_4', '1_6', '5_6', '1_4', '5_1', '1_1', '3_1', '0_4', '0_1', '1_7', '4_4', '2_1', '4_1', '2_7', '5_7', '5_4', '4_7', '0_7', '3_7']
+if __name__ == '__main__':
+    tokenizer = AutoTokenizer.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/models/switch-base-8"+ExpertManager.FTpwd+"-yrj")
+    model = SwitchTransformersForConditionalGenerationYRJ.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/models/switch-base-8"+ExpertManager.FTpwd+"-yrj", device_map="auto", offload_folder="offload")
+
+    output_lns= []
+    reference_lns = []
+    sum_cont= 0
+    for i, input in enumerate(inputSens):
+        if sum_cont >= 200:
+            break
+        # input_text = "summarize: "+input[textt]
+        input_text = input[textt]
+        if len(input_text.split(" "))< 300:
+            label_text = input["summary"]
+            input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+            input_ids = input_ids.to('cuda:0')
+            with torch.no_grad():  # Avoid saving intermediate layer outputs
+                outputs = model.generate(input_ids, max_new_tokens=100)
+            output_text = tokenizer.decode(outputs[0])
+            output_text=output_text.replace("<pad> ","")
+            output_text=output_text.replace("</s>","")
+            output_lns.append(output_text)
+            reference_lns.append(label_text)
+            sum_cont += 1
+    result = calculate_rouge(output_lns, reference_lns, use_stemmer=True)
+    print(result)
+    print("total cnt:", sum_cont)
