555801397ccdb3c7d56a261238b0c8ee034ad83f
Yeeethan00 <yirj0809@gmail.com>
2023-11-27 15:40:34 +0800

    style: energy

--- examples/research_projects/switch-base-generation/energy/switch_base_1token_GPU-energy.py
+++ examples/research_projects/switch-base-generation/energy/switch_base_1token_GPU-energy.py
@@ -7,7 +7,7 @@ import pyRAPL
 from pyRAPL import Measurement
 pyRAPL.setup()
 
-ExpertManager.Pwd = os.path.dirname(os.path.realpath(__file__))
+ExpertManager.Pwd = os.path.dirname(os.path.realpath(__file__)) + "/../"
 ExpertManager.Device = "cuda"
 ExpertManager.CacheLimit = 12
 ExpertManager.FTpwd = "-samsum"
@@ -17,8 +17,8 @@ base_num = 8
 
 ExpertManager.bwList = ['2_0', '3_5', '0_0', '0_5', '5_5', '4_0', '5_0', '4_5', '1_0', '4_3', '2_3', '3_3', '1_5', '3_2', '0_2', '0_3', '2_2', '4_2', '2_5', '3_0', '5_2', '1_2', '1_3', '5_3', '3_6', '4_6', '0_6', '2_6', '2_4', '3_4', '1_6', '5_6', '1_4', '5_1', '1_1', '3_1', '0_4', '0_1', '1_7', '4_4', '2_1', '4_1', '2_7', '5_7', '5_4', '4_7', '0_7', '3_7']
 if __name__ == '__main__':
-    tokenizer = AutoTokenizer.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/models/switch-base-"+str(base_num)+ExpertManager.FTpwd+"-yrj")
-    model = SwitchTransformersModelYRJ.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/models/switch-base-"+str(base_num)+ExpertManager.FTpwd+"-yrj", device_map="auto", offload_folder="offload")
+    tokenizer = AutoTokenizer.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/../models/switch-base-"+str(base_num)+ExpertManager.FTpwd+"-yrj")
+    model = SwitchTransformersModelYRJ.from_pretrained(os.path.dirname(os.path.realpath(__file__))+"/../models/switch-base-"+str(base_num)+ExpertManager.FTpwd+"-yrj", device_map="auto", offload_folder="offload")
 
     input_ids = tokenizer(
         "Studies have been shown that owning a dog is good for you", return_tensors="pt"
